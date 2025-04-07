3d73a0fcd73d73ab5925bc8a48ac91147475f1e4
saikrishnaporala <saikrishnaporala@gmail.com>
2025-04-08 01:12:07 +0530

    commit changes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/home', [VelzonRoutesController::class, 'landing'])->name('home');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     

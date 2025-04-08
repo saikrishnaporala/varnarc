2f5767a82208e1148a5a42bd1b676668808800d4
saikrishnaporala <saikrishnaporala@gmail.com>
2025-04-08 15:32:52 +0530

    css container update

--- resources/js/Pages/landing/index.vue
+++ resources/js/Pages/landing/index.vue
@@ -167,11 +167,10 @@ export default {
                 <BRow class="justify-content-center">
                     <BCol lg="8" sm="10">
                         <div class="text-center mt-lg-5 pt-5">
-                            <h1 class="display-6 fw-semibold mb-3 lh-base">The better way to manage your website with
-                                <span class="text-success">Velzon </span>
+                            <h1 class="display-6 fw-semibold mb-3 lh-base">Amplify Your Digital Presence with
+                                <span class="text-success">Varnarc </span>
                             </h1>
-                            <p class="lead text-muted lh-base">Velzon is a fully responsive, multipurpose and premium
-                                Bootstrap 5 Admin & Dashboard Template built in multiple frameworks.</p>
+                            <p class="lead text-muted lh-base">Unlock your brand’s full potential with our cutting-edge digital solutions. Whether you need a stunning website, a powerful marketing strategy, or seamless user experiences, we help you stand out in a competitive digital world. Elevate your online presence and drive meaningful engagement with strategies designed for success.</p>
 
                             <div class="d-flex gap-2 justify-content-center mt-4">
                                 <Link href="/auth/signin-basic" class="btn btn-primary">Get Started <i
